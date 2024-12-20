a1229c904547c96dd76ee56397106b9d0320a51f
Amar0911 <guptaamar0911@gmail.com>
2024-12-20 17:10:48 +0530

    update

--- prc.js
+++ prc.js
@@ -16,24 +16,94 @@
 // perform(x)
 
 
-function perform(params) {
-    document.getElementById('click')
-        .addEventListener('click', function () {
-            params();
-            if (!document.getElementById('unique-paragraph')) {
-                var p = document.createElement('p');
-                p.textContent = "Paragraph Created";
-                p.id = 'unique-paragraph'; 
-                document.body.appendChild(p);
-            } else {
-                console.log("Paragraph already exists");
-            }
-        });
+/* call back function */
+
+// function perform(params) {
+//     document.getElementById('click')
+//         .addEventListener('click', function () {
+//             params();
+//             if (!document.getElementById('unique-paragraph')) {
+//                 var p = document.createElement('p');
+//                 p.textContent = "Paragraph Created";
+//                 p.id = 'unique-paragraph'; 
+//                 document.body.appendChild(p);
+//             } else {
+//                 console.log("Paragraph already exists");
+//             }
+//         });
+// }
+
+// function x() {
+//     console.log('I am clicked');
+// }
+
+// perform(x);
+
+
+
+
+/* for loop */
+ 
+// var i = 1
+
+// for(i; i <=5; i++){
+//     console.log(i)
+// }
+
+
+
+/* call back in loop */
+
+// var num = [1,2,4,3]
+
+// for(let i of num){
+//     setTimeout(() => {
+//         console.log(i);
+//     }, i * 1000);
+// }
+
+
+/* Promise */
+
+const cart = ['shoe', 'tshirt', 'jeans']
+
+const pr = createOrder(cart).then(
+    function(orderId){
+        console.log('oder id is', orderId);
+        return makePayment(orderId)
+    }
+)
+.then(function(paymentStatus){
+    return placeOrder(paymentStatus)
+}
+)
+.catch(function(err){
+    console.log(err.message);
+})
+
+function createOrder(cart){
+    return new Promise(function(reslove, reject){
+        let orderId = '1';
+        reslove('Order Created!!')
+    })
 }
 
-function x() {
-    console.log('I am clicked');
+function makePayment(orderId){
+    return new Promise(function(reslove, reject){
+        let paymentStatus = true;
+        if(paymentStatus){
+            reslove(paymentStatus)
+        }
+        else{
+            const err = Error("Payment Failed")
+            reject(err)
+        }
+    })
 }
 
-perform(x);
+
+
+
+
+
 
